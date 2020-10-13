8d9057ee075232e31ed24b975a4173670963fc55
beamwanatvhaya <wanatchaya.co.61@ubu.ac.th>
2020-10-14 03:10:50 +0700

    bbbbb

--- fontend/src/view/home.js
+++ fontend/src/view/home.js
@@ -14,24 +14,25 @@ function Home() {
 
   return (
     <div>
-        <Button onClick={()=>{nextPage("BasicComputing")}} color="btn btn-secondary">
+        
+       <center> <Button onClick={()=>{nextPage("BasicComputing")}} color="btn btn-outline-success">
             <h5>1.Basic Computing</h5>
         </Button><br/><br/>
-        <Button onClick={()=>{nextPage("LinearEquations")}} color="btn btn-secondary">
+        <Button onClick={()=>{nextPage("LinearEquations")}} color="btn btn-outline-success">
             <h5>2.Linear Equations</h5>
         </Button><br/><br/>
-        <Button onClick={()=>{nextPage("Interpolation")}} color="btn btn-secondary">
+        <Button onClick={()=>{nextPage("Interpolation")}} color="btn btn-outline-success">
             <h5>3.Interpolation	</h5>
         </Button><br/><br/>
-        <Button onClick={()=>{nextPage("Differentiation")}} color="btn btn-secondary">
+        <Button onClick={()=>{nextPage("Differentiation")}} color="btn btn-outline-success">
             <h5>4.Differentiation</h5>
         </Button><br/><br/>
-        <Button onClick={()=>{nextPage("Integration")}} color="btn btn-secondary">
+        <Button onClick={()=>{nextPage("Integration")}} color="btn btn-outline-success">
             <h5>5.Integration</h5>
         </Button><br/><br/>
-        <Button onClick={()=>{nextPage("Rootfinding")}} color="btn btn-secondary">
+        <Button onClick={()=>{nextPage("Rootfinding")}} color="btn btn-outline-success">
             <h5>6.Root-finding</h5>
-        </Button><br/>
+        </Button><br/></center>
     </div>
   );
 }
