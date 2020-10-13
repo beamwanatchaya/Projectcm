077083ac2121c796044978c1984a02b302149637
beamwanatvhaya <wanatchaya.co.61@ubu.ac.th>
2020-10-14 01:55:35 +0700

    Updates

---
+++ precache-manifest.b3d6ceaa1019d1438f97ac1a05cb416e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd60b055bfe7a93a556107dab3859936",
+    "url": "/Projectcm/index.html"
+  },
+  {
+    "revision": "c1d9291b5070c7091832",
+    "url": "/Projectcm/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "19a1e6166864f58ff2f9",
+    "url": "/Projectcm/static/js/2.a693db1c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Projectcm/static/js/2.a693db1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1d9291b5070c7091832",
+    "url": "/Projectcm/static/js/main.c1df44f0.chunk.js"
+  },
+  {
+    "revision": "ee34143252c6f34cea1c",
+    "url": "/Projectcm/static/js/runtime-main.8695b2de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Projectcm/static/media/logo.5d5d9eef.svg"
+  }
+]);
